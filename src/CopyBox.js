7583203434d7e40f4f90a63d669073d5c02ed519
Benjamin Tatum <bentatum@me.com>
2016-05-11 15:03:46 -0400

    0.0.1

--- src/CopyBox.js
+++ src/CopyBox.js
@@ -1,22 +1,51 @@
-import { default as React, createElement, PropTypes } from 'react'
-import { Flex } from 'reflexbox'
+import { default as React, Component, createElement, PropTypes } from 'react'
 
-const CopyBox = ({ buttonIs, buttonProps, inputIs, inputProps }) =>
-  <Flex>
-    {createElement(inputIs, inputProps)}
-    {createElement(buttonIs, buttonProps)}
-  </Flex>
+export default class CopyBox extends Component {
 
-CopyBox.defaultProps = {
-  buttonIs: 'button',
-  inputIs: 'input'
+  static defaultProps = {
+    buttonIs: 'button',
+    buttonProps: {},
+    inputIs: 'input',
+    inputProps: {}
+  };
+
+  static propTypes = {
+    buttonIs: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+    buttonProps: PropTypes.object,
+    inputIs: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+    inputProps: PropTypes.object
+  };
+
+  constructor (props) {
+    super(props)
+    this.state = { success: false }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    const { input } = this.refs
+    input.setSelectionRange(0, input.value.length)
+    document.execCommand('copy')
+    this.setState({ success: true })
+  }
+
+  render () {
+    const { buttonIs, buttonProps, inputIs, inputProps } = this.props
+    return (
+      <div>
+        {createElement(inputIs, {
+          ref: 'input',
+          type: 'text',
+          value: 'Copy this text by clicking it',
+          ...inputProps,
+          onFocus: this.handleClick
+        })}
+        {createElement(buttonIs, {
+          children: !this.state.success ? 'Click to copy' : 'Copied',
+          ...buttonProps,
+          onClick: this.handleClick
+        })}
+      </div>
+    )
+  }
 }
-
-CopyBox.propTypes = {
-  buttonIs: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
-  buttonProps: PropTypes.object,
-  inputIs: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
-  inputProps: PropTypes.object
-}
-
-export default CopyBox
